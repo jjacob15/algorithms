bbfac8e37f1bd97a556d3726250c8aaa4ab64834
jaison jacob <jaison.jack@gmail.com>
2021-04-15 13:42:49 +0800

    positional list ADT with ability to add before or after node

---
+++ algorithms/linked-list/doubly-linked-list/positional_list.py
@@ -0,0 +1,82 @@
+from doublylinkedbase import _DoublyLinkedBase
+
+# PositionalList validates the element when trying to insert before or after to check if it belongs to the same list.
+# good example where you are not using the node to keep the details of which list does it belongs too (container)
+class PositionalList(_DoublyLinkedBase):
+    class Position:
+        def __init__(self, container, node):
+            self._container = container  # holds the positionalList reference for integrity
+            self._node = node
+
+        def element(self):
+            return self._node._element
+
+        def __eq__(self, other):
+            return type(other) is type(self) and other._node is self._node
+
+        def __ne__(self, other):
+            return not (self == other)  # opposite of __eq__
+
+    def _validate(self, p):
+        if not isinstance(p, self.Position):
+            raise TypeError('p must be a proper Postion type')
+        if p._container is not self:  # check if the container is the same
+            raise ValueError('p does not belong in this container')
+        if p._node._next is None:
+            raise ValueError('p is no longer valid')
+
+        return p._node
+
+    def _make_position(self, node):
+        if node is self._header or node is self._trailer:
+            return None
+        else:
+            return self.Position(self, node)
+
+    def first(self):
+        return self._make_position(self._header._next)
+
+    def last(self):
+        return self._make_position(self._trailer._prev)
+
+    def before(self, p):
+        node = self._validate(p)
+        return self._make_position(node._prev)
+
+    def after(self, p):
+        node = self._validate(p)
+        return self._make_position(node._next)
+
+    def __iter__(self):
+        cursor = self.first()
+        while cursor is not None:
+            yield cursor.element()
+            cursor = self.after(cursor)
+
+    def _insert_between(self, e, predecessor, successor):
+        node = super()._insert_between(e, predecessor, successor)
+        return self._make_position(node)
+
+    def add_first(self, e):
+        return self._insert_between(e, self._header, self._header._next)
+
+    def add_last(self, e):
+        return self._insert_between(e, self._trailer._prev, self._trailer)
+
+    def add_before(self, p, e):
+        original = self._validate(p)
+        return self._insert_between(e, original._prev, original)
+
+    def add_after(self, p, e):
+        original = self._validate(p)
+        return self._insert_between(e, original, original._next)
+
+    def delete(self, p):
+        original = self._validate(p)
+        return self._delete_node(original)
+
+    def replace(self, p, e):
+        original = self._validate(p)
+        old_value = original._element
+        original._element = e
+        return old_value
