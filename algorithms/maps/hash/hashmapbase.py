4aae745f977f39b5323a82b6f67d40da67813aec
jjacob15 <jaison.jack@gmail.com>
2021-06-22 10:23:56 +0800

    move

---
+++ algorithms/maps/hash/hashmapbase.py
@@ -0,0 +1,42 @@
+from random import randrange
+from .mapbase import MapBase
+
+
+class HashMapBase(MapBase):
+    def __init__(self,cap = 11,p = 109345121):
+        self._table = cap * [None]
+        self._n =0 # number of entries in the map
+        self._prime = p  # prime for MAD compression
+        self._scale = 1 + randrange(p-1) #scale from 1 to p-1 for MAD
+        self._shift = randrange(p) #shift from to p-1 for MAD
+
+    def _hash_function(self,k):
+        return (hash(k)*self._scale + self._shift) % self._prime % len(self._table)
+    
+    def __len__(self):
+        return self._n
+
+    def __getitem__(self,k):
+        j = self._hash_function(k)
+        return self._bucket_getitem(j,k)
+
+    def __setitem__(self,k,v):
+        j = self._hash_function(k)
+        self._bucket_setitem(j,k,v)
+        if self._n > len(self._table) // 2:
+            self._resize(2 * len(self._table)-1) #number 2 ^ x -1 is often prime but not always
+
+    def __delitem__(self,k):
+        j = self._hash_function(k)
+        self._bucket_delitem(j,k)
+        self._n -=1
+
+    def _resize(self,c):
+        print('old len',len(self._table))
+        print('used old len',len(self))
+        print('new len',c)
+        old = list(self.items())
+        self._table = c * [None]
+        self._n = 0
+        for (k,v) in old:
+            self[k] = v
