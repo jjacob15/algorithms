b83f6a3a6dfb386d0dd7ac0eac6bf87066607539
jjacob15 <jaison.jack@gmail.com>
2021-07-12 10:39:34 +0800

    minor format

--- algorithms/sorting/mergesort.py
+++ algorithms/sorting/mergesort.py
@@ -7,25 +7,24 @@ def merge(S1, S2, S):
         else:
             S[i+j] = S2[j]
             j += 1
-    print('merge', S1, S2)
+    print('merging', S1, S2, '-->', S)
 
 
 def merge_sort(S, msg):
-    print(msg, S)
+    print('-->' if msg is None else msg, S)
     n = len(S)
-    if n <2:
+    if n < 2:
         return
-    mid = n //2
+    mid = n // 2
     S1 = S[0:mid]
     S2 = S[mid:n]
-    merge_sort(S1,'S1')
+    merge_sort(S1, 'S1')
     merge_sort(S2, 'S2')
     merge(S1, S2, S)
 
 
 if __name__ == "__main__":
-    S = [85,24,63,45,17,31,96,50]
+    S = [85, 24, 63, 45, 17, 31, 96, 50]
     # S = [8,3,2,3,4,6,6,4,2,34,4,5,5,435,23,4,4,34,235,45,3242,425,4,54,45,8,3,2,3,4,6,6,4,2,34,4,5,5,435,23,4,4,34,235,45,3242,425,4,54,45,8,3,2,3,4,6,6,4,2,34,4,5,5,435,23,4,4,34,235,45,3242,425,4,54,45,8,3,2,3,4,6,6,4,2,34,4,5,5,435,23,4,4,34,235,45,3242,425,4,54,45]
-    merge_sort(S,'')
+    merge_sort(S,None)
     print(S)
-
