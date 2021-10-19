4e42fbf1b74090e8f8930888b24796af6f0ed306
jjacob15 <jaison.jack@gmail.com>
2021-10-19 11:14:29 +0800

    adding print

--- algorithms/tree/simplebinarytree.py
+++ algorithms/tree/simplebinarytree.py
@@ -1,3 +1,5 @@
+import math
+
 
 class SimpleBinaryTree:
     class _Node:
@@ -8,6 +10,13 @@ class SimpleBinaryTree:
             self._parent = parent
             self._left = left
             self._right = right
+
+    class _PrintNode:
+        __slots__ = '_node', '_level'
+
+        def __init__(self, node, level=0):
+            self._node = node
+            self._level = level
 
     def __init__(self):
         self._root = None
@@ -21,7 +30,7 @@ class SimpleBinaryTree:
         if node._left is None:
             return None
         return node._left
-    
+
     def right(self, node):
         if node._right is None:
             return None
@@ -30,7 +39,7 @@ class SimpleBinaryTree:
     def add_root(self, elem):
         self._root = self._Node(elem)
         return self._root
-    
+
     def add_left(self, elem, parent):
         parent._left = self._Node(elem, parent)
         return parent._left
@@ -57,22 +66,48 @@ class SimpleBinaryTree:
             self.postorder(self.right(node))
             print(node._element, end="", flush=True)
 
+    def print(self):
+        queue = []
+        queue.append(self._PrintNode(self.root()))
+        currentLevel = 0
+        while len(queue) != 0:
+            printNode = queue.pop(0)
+            node = printNode._node
+            if(currentLevel != printNode._level):
+                print()
+            print(str(node._element) + ' ',  end="", flush=True)
+            currentLevel = printNode._level
 
+            if self.left(node):
+                queue.append(self._PrintNode(
+                    self.left(node), printNode._level+1))
+            if self.right(node):
+                queue.append(self._PrintNode(
+                    self.right(node), printNode._level+1))
 
 
 if __name__ == "__main__":
     tree = SimpleBinaryTree()
-    root = tree.add_root(1)
-    two = tree.add_left(2, root)
-    three = tree.add_right(3, root)
-    tree.add_left(4,two)
-    tree.add_right(5,two)
+    root = tree.add_root(1) #0
+    two = tree.add_left(2, root) #1
+    three = tree.add_right(3, root) #1
 
-    print('Printing inorder')
-    tree.inorder(root)
-    print("")
-    print('Printing preorder')
-    tree.preorder(root)
-    print("")
-    print('Printing postorder')
-    tree.postorder(root)
+    four = tree.add_left(4, two) #2
+    five = tree.add_right(5, two) #2
+
+    tree.add_left(6, four) #3
+    tree.add_right(7, four) #3
+    
+    tree.add_left(8, five) #3
+    tree.add_right(9, five) #3
+
+    tree.print()
+
+    # print('Printing inorder')
+    # tree.inorder(root)
+    # print("")
+    # print('Printing preorder')
+    # tree.preorder(root)
+    # print("")
+    # print('Printing postorder')
+    # tree.postorder(root)
